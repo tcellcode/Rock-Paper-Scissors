21f78ff9d34bb468084e698dc60f4828f6896fa7
tcellcode <99007786+tcellcode@users.noreply.github.com>
2022-09-05 12:43:22 +0700

    Update improve_gui.py

--- Rock Paper Scissor/improve_gui.py
+++ Rock Paper Scissor/improve_gui.py
@@ -1,7 +1,7 @@
 from numpy import random
 from tkinter import *
 from PIL import Image, ImageTk
-import os; os.chdir(os.path.dirname(os.path.abspath(__file__))) # change cwd
+import os; os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def rock_paper_scissors(choice1, choice2):
     if choice1 == choice2: return 0.5 # tie
